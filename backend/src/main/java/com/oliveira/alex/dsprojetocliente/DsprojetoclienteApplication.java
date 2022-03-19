22c3ed0233d2519044efbf93eee992abbc432317
Alex Oliveira <alex_oliveira08@yahoo.com.br>
2022-03-19 18:26:13 -0300

    Entity created

---
+++ backend/src/main/java/com/oliveira/alex/dsprojetocliente/DsprojetoclienteApplication.java
@@ -0,0 +1,13 @@
+package com.oliveira.alex.dsprojetocliente;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsprojetoclienteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DsprojetoclienteApplication.class, args);
+	}
+
+}
