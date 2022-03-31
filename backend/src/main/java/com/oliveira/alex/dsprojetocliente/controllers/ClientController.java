5d06b2b8f3da8f06205fde2bcc7df1bed1c76683
Alex Oliveira <alex_oliveira08@yahoo.com.br>
2022-03-30 22:46:19 -0300

    Method Put, Delete

--- backend/src/main/java/com/oliveira/alex/dsprojetocliente/controllers/ClientController.java
+++ backend/src/main/java/com/oliveira/alex/dsprojetocliente/controllers/ClientController.java
@@ -5,9 +5,11 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort.Direction;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -19,36 +21,45 @@ import com.oliveira.alex.dsprojetocliente.services.ClientService;
 @RestController
 @RequestMapping(value = "/clients")
 public class ClientController {
-	
+
 	@Autowired
 	private ClientService service;
-	
+
 	@GetMapping
-	public ResponseEntity<Page<ClientDTO>> findAll(
-			@RequestParam(value="page", defaultValue="0") Integer page,
-			@RequestParam(value="linesPerpage", defaultValue="5") Integer linePerPage,
-			@RequestParam(value="orderBy", defaultValue="name") String orderBy,
-			@RequestParam(value="direction", defaultValue="ASC") String direction
-			){		
-		
-		PageRequest pageRequest = PageRequest.of(page, linePerPage, Direction.valueOf(direction), orderBy);
-		
+	public ResponseEntity<Page<ClientDTO>> findAll(@RequestParam(value = "page", defaultValue = "0") Integer page,
+			@RequestParam(value = "linesPerPage", defaultValue = "5") Integer linesPerPage,
+			@RequestParam(value = "orderBy", defaultValue = "name") String orderBy,
+			@RequestParam(value = "direction", defaultValue = "ASC") String direction) {
+
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+
 		Page<ClientDTO> list = service.findAllPaged(pageRequest);
 		return ResponseEntity.ok().body(list);
-		
+
 	}
-	
+
 	@GetMapping(value = "/{id}")
-	public ResponseEntity<ClientDTO> findaById(@PathVariable Long id){
+	public ResponseEntity<ClientDTO> findaById(@PathVariable Long id) {
 		ClientDTO cli = service.findById(id);
 		return ResponseEntity.ok().body(cli);
 	}
-	
-	
+
 	@PostMapping
-	public ResponseEntity<ClientDTO> insert(@RequestBody ClientDTO dto){
+	public ResponseEntity<ClientDTO> insert(@RequestBody ClientDTO dto) {
 		dto = service.insert(dto);
 		return ResponseEntity.ok().body(dto);
 	}
+
+	@PutMapping(value = "/{id}")
+	public ResponseEntity<ClientDTO> update(@PathVariable Long id, @RequestBody ClientDTO dto) {
+		dto = service.update(id, dto);
+		return ResponseEntity.ok().body(dto);
+	}
 	
+	@DeleteMapping(value = "/{id}")
+	public ResponseEntity<ClientDTO> delete(@PathVariable Long id) {
+		service.delete(id);
+		return ResponseEntity.ok().build();
+	}
+
 }
--- backend/src/main/java/com/oliveira/alex/dsprojetocliente/services/ClientService.java
+++ backend/src/main/java/com/oliveira/alex/dsprojetocliente/services/ClientService.java
@@ -2,6 +2,8 @@ package com.oliveira.alex.dsprojetocliente.services;
 
 import java.util.Optional;
 
+import javax.management.RuntimeErrorException;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
@@ -14,19 +16,19 @@ import com.oliveira.alex.dsprojetocliente.repositories.ClientRepository;
 
 @Service
 public class ClientService {
-	
+
 	@Autowired
 	private ClientRepository repository;
-	
+
 	@Transactional(readOnly = true)
-	public Page<ClientDTO> findAllPaged(PageRequest pageRequest){
+	public Page<ClientDTO> findAllPaged(PageRequest pageRequest) {
 		Page<Client> list = repository.findAll(pageRequest);
-		
-		//Method Lambda
+
+		// Method Lambda
 		Page<ClientDTO> listDto = list.map(x -> new ClientDTO(x));
 		return listDto;
-		
-		//Method using For
+
+		// Method using For
 //		List<ClientDTO> listDto = new ArrayList<>();
 //			
 //		for(Client cli : list) {
@@ -35,7 +37,7 @@ public class ClientService {
 //		}
 //		
 //		return listDto;
-}
+	}
 
 	@Transactional(readOnly = true)
 	public ClientDTO findById(Long id) {
@@ -43,7 +45,7 @@ public class ClientService {
 		Client entity = obj.get();
 		return new ClientDTO(entity);
 	}
-	
+
 	@Transactional
 	public ClientDTO insert(ClientDTO dto) {
 		Client entity = new Client();
@@ -52,12 +54,33 @@ public class ClientService {
 		entity.setIncome(dto.getIncome());
 		entity.setBirthDate(dto.getBirthDate());
 		entity.setChildren(dto.getChildren());
-		
+
 		entity = repository.save(entity);
-		
+
 		return new ClientDTO(entity);
 	}
 
-	
+	@Transactional
+	public ClientDTO update(Long id, ClientDTO dto) {
+		Client entity = repository.getOne(id);
+		entity.setName(dto.getName());
+		entity.setCpf(dto.getCpf());
+		entity.setIncome(dto.getIncome());
+		entity.setBirthDate(dto.getBirthDate());
+		entity.setChildren(dto.getChildren());
+
+		entity = repository.save(entity);
+		return new ClientDTO(entity);
+
+	}
+
+	public void delete(Long id) {
+		try {
+			repository.deleteById(id);
+		} catch (Exception e) {
+			throw new RuntimeErrorException(null, e.getMessage());
+		}
+
+	}
 
 }
